5e4171c4a4642a1a2f33ed964bfdea8d90c2c54c
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2020-03-28 20:43:27 -0300

    Criando operação de instanciação

--- src/main/java/com/sparsis/modelagem_conceitual/ModelagemConceitualApplication.java
+++ src/main/java/com/sparsis/modelagem_conceitual/ModelagemConceitualApplication.java
@@ -1,13 +1,31 @@
 package com.sparsis.modelagem_conceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemConceitualApplication {
+import com.sparsis.modelagem_conceitual.domain.Categoria;
+import com.sparsis.modelagem_conceitual.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemConceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
