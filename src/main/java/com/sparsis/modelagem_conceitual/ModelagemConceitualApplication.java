3756cc3c2dc32b52d86e2f4ed0d000daa2a3e816
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2020-03-28 21:07:42 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/sparsis/modelagem_conceitual/ModelagemConceitualApplication.java
+++ src/main/java/com/sparsis/modelagem_conceitual/ModelagemConceitualApplication.java
@@ -1,5 +1,6 @@
 package com.sparsis.modelagem_conceitual;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sparsis.modelagem_conceitual.domain.Categoria;
+import com.sparsis.modelagem_conceitual.domain.Produto;
 import com.sparsis.modelagem_conceitual.repository.CategoriaRepository;
+import com.sparsis.modelagem_conceitual.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemConceitualApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -25,7 +31,21 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
+		
+		
+		Produto produto1 = new Produto(null, "Computador", new BigDecimal("2000"));
+		Produto produto2 = new Produto(null, "Impressora", new BigDecimal("800"));
+		Produto produto3 = new Produto(null, "Mouse", new BigDecimal("80"));
+		
+		categoria1.setProdutos(Arrays.asList(produto1, produto2, produto3));
+		categoria2.setProdutos(Arrays.asList(produto2));
+		
+		produto1.setCategorias(Arrays.asList(categoria1));
+		produto2.setCategorias(Arrays.asList(categoria1, categoria2));
+		produto3.setCategorias(Arrays.asList(categoria1));
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 
 }
--- src/main/java/com/sparsis/modelagem_conceitual/domain/Categoria.java
+++ src/main/java/com/sparsis/modelagem_conceitual/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.sparsis.modelagem_conceitual.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Categoria implements Serializable {
 	
 	@Column(name = "nome")
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 	
@@ -40,6 +46,14 @@ public class Categoria implements Serializable {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
