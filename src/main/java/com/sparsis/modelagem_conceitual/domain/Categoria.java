a0c2e329981fd4aabb3287729b0af738d471009b
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2020-03-28 23:35:02 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/sparsis/modelagem_conceitual/domain/Categoria.java
+++ src/main/java/com/sparsis/modelagem_conceitual/domain/Categoria.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable {
@@ -25,6 +27,7 @@ public class Categoria implements Serializable {
 	@Column(name = "nome")
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
